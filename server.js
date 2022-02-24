87fb6cc1226644ea5ad3f4d554d088fea20a295d
AzizHannafi <hannafimedaziz@gmail.com>
2022-02-24 11:42:44 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const app= express()
+const server = require('http').Server(app)
+const port = process.env.PORT || 3031
+const cors = require("cors");
+
+
+app.use(cors());
+
+
+const io = require('socket.io')(server,{
+    cors:{
+        origin: "*",
+        methods: ["GET","POST"],
+    }
+})
+const { ExpressPeerServer } = require('peer')
+const peerServer = ExpressPeerServer(server,{
+    debug: true
+})
+
+app.use('/',(req,res)=>{
+    res.status(200).send("Welcome to our Stream server Cool 😎🔥🔥🔥")
+})
+
+io.on('connection', socket => {
+  
+    socket.on('join-room', (roomId, userId) => {
+      console.log("user "+userId+ " joined room "+roomId)
+      socket.join(roomId)
+      socket.broadcast.to(roomId).emit('user-connected', userId);
+   
+    })
+  })
+  
+
+
+server.listen(port,()=>{
+  console.log('Stream sever running at port '+port)
+})
+
